ab8ff8d8b7d2915d3d5c3a53b80d223616fdf7f2
Luis J Feliz <lfjo.mobil@gmail.com>
2018-08-04 16:04:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function shout(string) {
   return string.toUpperCase()
 }
@@ -7,9 +6,26 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logshout(string) {
-  console.log('HELLO')
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-  console.log('hello')
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
+}
